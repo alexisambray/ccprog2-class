87b84729880357c59da40c2db8dd8f5aee9b535b
Alexis Ambray <alexisambray@gmail.com>
2021-08-16 17:21:10 +0800

    Sample 2

---
+++ grades/sample-structures-2.c
@@ -0,0 +1,30 @@
+/* Example program on structures demonstrating the following:
+    a. nested struct data type declaration
+    b. initialize the struct members via direct assignment operation = strcpy() for strings
+    c. use scanf() to input/initialize the contents of structure members
+*/
+#include <stdio.h>  // < > pre-defined o system-defined header file
+#include <string.h>
+#include "grades.h"  // " " this is how you include user-defined header file
+
+int main() {
+  struct studentTag temp;
+
+  // Q1: What is the output of the value temp variables?
+  // A1: garbage
+
+  // use scanf() to input/initialize the contents of the structure variable
+  scanf("%s", temp.name.last);        // input last name
+  scanf("%s", temp.name.first);       // input first name
+  scanf("%d", &temp.practice_score);  // input practice score
+  scanf("%d", &temp.midterm_score);   // input midterm score
+
+  // print the values of the members of temp using the structure member operator
+  // (dot)
+  printf("%s\n", temp.name.last);   // last name of the student
+  printf("%s\n", temp.name.first);  // first name of the student
+  printf("%d\n", temp.practice_score);
+  printf("%d\n", temp.midterm_score);
+
+  return 0;
+}
