7520d891c567c64549f30011435a213528792da0
Alexis Ambray <alexisambray@gmail.com>
2021-08-30 17:30:21 +0800

    Added employeeTag structure

--- examples/struct-5.c
+++ examples/struct-5.c
@@ -1,5 +1,8 @@
 /* Listing 3.5: Passing a Structure Pointer as Parameter */
 #include <stdio.h>
+#include <string.h>
+
+typedef char String[30];
 
 // take note of this global declaration
 struct dateTag {
@@ -8,13 +11,23 @@ struct dateTag {
   int year;
 };
 
+struct employeeTag {
+  String firstName[20];
+  String lastName[20];
+};
+
 void InputDate(struct dateTag* ptr) {
   printf("Input month, day and year: ");
   scanf("%d %d %d", &ptr->month, &ptr->day, &ptr->year);
 }
 
-void OutputDate(struct dateTag* ptr) {
-  printf("%d/%d/%d", ptr->month, ptr->day, ptr->year);
+void InputEmployee(struct employeeTag* ptr) {
+  printf("\nInput first name and last name: ");
+  scanf("%s %s", ptr->firstName, ptr->lastName);
+}
+
+void PrintEmployee(struct employeeTag* ptr) {
+  printf("%s %s", ptr->firstName, ptr->lastName);
 }
 
 void PrintDate(struct dateTag* ptr) {
@@ -23,10 +36,15 @@ void PrintDate(struct dateTag* ptr) {
 
 int main() {
   struct dateTag today;
+  struct employeeTag name;
 
   InputDate(&today);
   printf("Today's date is ");
   PrintDate(&today);
+
+  InputEmployee(&name);
+  printf("Employee's name is ");
+  PrintEmployee(&name);
 
   return 0;
 }
